6f61525b035e0d442d1969bf476f02ec22d3319d
hooman734 <hesamyan@outlook.com>
2020-08-17 20:23:30 +0400

    committing html

---
+++ src/HowTo.svelte
@@ -0,0 +1,9 @@
+<style>
+    img {
+        max-width: 100%;
+    }
+</style>
+
+<div bp="grid">
+    <img bp="offset-5@md 4@md 12@sm" src="handwashing-poster.png" alt="How to wash">
+</div>
---
+++ src/Timer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import ProgressBar from './ProgressBar.svelte';
+</script>
+<style>
+    h2 {
+        margin: 0;
+    }
+    .start {
+        background-color: deeppink;
+        width: 100%;
+        height: 4em;
+        margin: 10px 0;
+        cursor: pointer;
+    }
+</style>
+<div bp="grid">
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+</div>
+<ProgressBar></ProgressBar>
+<button class="start">Start</button>
